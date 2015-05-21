2c4c694c7c15f8f90c9468be45ac48789725dc8f
Sukoreno Mukti <sukorenomw@gmail.com>
2015-05-21 10:06:56 +0700

    added file validation for image only and size not to greater than 200kb

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,12 @@ class Image < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	
   	validates :title, presence: true
-  	validates :image, presence: true
+  	validates :image, 
+  			  presence: true
+  	validate :file_size
+
+  	def file_size
+		errors[:image] << "You cannot upload a file greater than 200kb" if image.size > 0.2.megabytes
+	end
 
 end
